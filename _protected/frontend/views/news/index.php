15ed7da698d0a49228c09e3bdf081eb5c067ed37
bswify <34600468+bswify@users.noreply.github.com>
2018-06-19 13:07:28 +0700

    bee FE news

--- _protected/frontend/views/news/index.php
+++ _protected/frontend/views/news/index.php
@@ -36,23 +36,5 @@ $this->params['breadcrumbs'][] = $this->title;
 
 
 
-        <div class="col-md-6 ml-auto mr-auto">
-            <h4 class="title text-center">บริการของเรา</h4>
-            <div class="nav-align-center">
-                <ul class="nav nav-pills nav-pills-primary" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" data-toggle="tab" href="#room" role="tablist">
-                            <i class="fas fa-bed"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " data-toggle="tab" href="#food" role="tablist">
-                            <!--                                <i class=" glyphicon-cutlery"></i>-->
-                            <i class="fas fa-utensils"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
 
 </div>
