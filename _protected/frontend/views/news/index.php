9da17cb3f335799f2198b293c0f6855d9bbd64e6
bswify <34600468+bswify@users.noreply.github.com>
2018-06-19 13:46:31 +0700

    bee แก้ไขหน้าแรก ของข่าว

--- _protected/frontend/views/news/index.php
+++ _protected/frontend/views/news/index.php
@@ -19,22 +19,61 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create News', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+<!--    = GridView::widget([-->
+<!--        'dataProvider' => $dataProvider,-->
+<!--        'filterModel' => $searchModel,-->
+<!--        'columns' => [-->
+<!--            ['class' => 'yii\grid\SerialColumn'],-->
+<!---->
+<!--            'Nid',-->
+<!--            'Ntopic:ntext',-->
+<!--            'Ndes:ntext',-->
+<!--            'Nimg:ntext',-->
+<!---->
+<!--            ['class' => 'yii\grid\ActionColumn'],-->
+<!--        ],-->
+<!--    ]); ?>-->
 
-            'Nid',
-            'Ntopic:ntext',
-            'Ndes:ntext',
-            'Nimg:ntext',
+    <?php
+//    $newQuery = clone $dataProvider->query;
+//    $model = $newQuery->limit(1)->one();
+    foreach( $dataProvider->models as $myModel) {
+        ?>
+        <div class="col-xs-12 col-sm-6  col-md-6 col-lg-6 ">
+            <div class="card" data-turbolinks="false">
+                <div class="thumbnail">
+                    <img src="<?= Yii::getAlias('@ShowN') . $myModel['Nimg'] ?>"
+                         data-retina="<?= Yii::getAlias('@ShowN') . $myModel['Nimg']  ?>"
+                         alt="No Image">
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+                    <a href="#" class="thumb-cover"></a>
+                    <b class="actions">
+                        <a href="#" class="btn btn-neutral btn-round btn-fill" rel="tooltip"
+                           title=""
+                           data-original-title="รายละเอียด">
+                            <i class="fa fa-align-left"></i>
+                        </a>
+                       </b>
 
+                </div>
+                <div class="card-info">
 
+                    <a href="/product/paper-dashboard-2-pro">
+                        <h3><?php
+                            $myModel['Ntopic']
+                            ?>
+                        </h3>
 
+                        <p><?php
+                            $myModel['Ndes']
+                            ?> </p>
+                    </a>
+                    
+                </div>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
 
 </div>
