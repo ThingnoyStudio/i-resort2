9a62ac9c34b36c6115ce4f5329e26bcad43949a5
bswify <34600468+bswify@users.noreply.github.com>
2018-06-23 10:11:06 +0700

    bee

--- _protected/backend/views/users/index.php
+++ _protected/backend/views/users/index.php
@@ -56,7 +56,40 @@ $this->params['breadcrumbs'][] = $this->title;
 //            'userstatus.USname',
             //'iduser',
 
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'header' => "การทำงาน",
+                'options' => ['style' => 'width:135px;'],
+                'headerOptions' => ['class' => 'text-center'],
+                'template' => '<div class="btn-group btn-group-sm" role="group" aria-label="..."> {view} {update} {delete} </div>',
+                'buttons' => [
+                    'view' => function ($url, $model, $key) {
+                        return Html::a('<i class="fa fa-eye"></i>', $url,
+                            ['title' => 'View user',
+                                'class' => 'btn btn-success',
+                                'id' => 'actioncol',
+                                'style' => 'padding: 5px 10px;    border-right: 2px solid #d4d4e0ab;']);
+                    },
+                    'update' => function ($url, $model, $key) {
+                        return Html::a('<i class="fa fa-pencil"></i>', $url,
+                            ['title' => 'Edit user',
+                                'class' => 'btn btn-success',
+                                'id' => 'actioncol',
+                                'style' => 'padding: 5px 10px;    border-right: 2px solid #d4d4e0ab;']);
+                    },
+                    'delete' => function ($url, $model, $key) {
+                        return Html::a('<i class="fa fa-trash"></i>', $url, [
+                            'title' => Yii::t('yii', 'Delete user'),
+                            'data-confirm' => Yii::t('yii', 'คุณต้องการลบรายการนี้หรือไม่?'),
+                            'data-method' => 'post',
+                            'data-pjax' => '0',
+                            'class' => 'btn btn-success',
+                            'id' => 'actioncol',
+                            'style' => 'padding: 5px 10px;    border-right: 2px solid #d4d4e0ab;'
+                        ]);
+                    }
+                ]
+            ],// ActionColumn
         ],
     ]); ?>
 </div>
